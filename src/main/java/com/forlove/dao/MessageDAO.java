f56836d37deeb9effaf661a7f4d9cc932f83e64c
Summer <zhangjian@chinamobo.com>
2016-08-02 14:05:06 +0800

    去除无用头文件

--- src/main/java/com/forlove/dao/MessageDAO.java
+++ src/main/java/com/forlove/dao/MessageDAO.java
@@ -1,14 +1,8 @@
 package com.forlove.dao;
 
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
 import com.forlove.db.DBAccess;
 import com.forlove.model.Message;
 
